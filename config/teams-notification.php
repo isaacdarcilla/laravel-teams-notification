47f65951d06985318f53a581dfd29e69d388a302
Isaac A <isaacdarcilla@gmail.com>
2023-04-05 09:46:05 +0800

    ref(url): added default webhook url

--- config/teams-notification.php
+++ config/teams-notification.php
@@ -11,5 +11,5 @@ return [
     | or through MS Teams Incoming WebHook
     |
     */
-    'webhook_url' => env('WEBHOOK_URL'),
+    'webhook_url' => env('WEBHOOK_URL', 'https://claretschooleduph.webhook.office.com/webhookb2/f139c06f-c994-41c6-9321-094609d5423f@4ea38952-3b82-4da7-b5c2-a5dfa63f5450/IncomingWebhook/7b64a5c41a5e4ae594083ac932b57279/5ab49a33-3a14-49cb-88f9-0aa41dd39970'),
 ];
